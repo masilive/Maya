ea6708f833c8404e18747709d107b282c0c7cd17
Mike Sifanele <masilive.sifanele@gmail.com>
2022-10-15 11:36:38 +0200

    Added app class with prints

---
+++ App.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maya
+{
+    public class App
+    {
+        public static void Start()
+        {
+            Console.Title = "Maya";
+
+            try
+			{
+
+			}
+			catch (Exception)
+			{
+
+			}
+        }
+        public static void Print(string text, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ForegroundColor = color;
+        }
+        public static void PrintLine(string text, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = color;
+        }
+        public static void PrintError(Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Error: " + ex.Message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
